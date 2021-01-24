cc40f80fe616af59bc39348affc8d48a0f7c3a00
ytyaru <yttry0@gmail.com>
2021-01-24 16:19:28 +0900

    name

---
+++ docs/2/radio.js
@@ -0,0 +1,27 @@
+class Radio {
+    static parse() {
+        const radios = document.querySelectorAll('radio');
+        for (let radio of radios) {
+            console.log(radio);
+            Radio.#createRadioButton(radio);
+        }
+    }
+    static #createRadioButton(radio) {
+        const options = radio.querySelectorAll('option');
+        for (let option of options) {
+            const id = radio.getAttribute('id')+'-'+(option.getAttribute('name')||option.getAttribute('value'));
+            const input = document.createElement('input');
+            input.setAttribute('type', 'radio');
+            input.setAttribute('value', option.getAttribute('value'));
+            input.setAttribute('name', radio.getAttribute('id'));
+            input.setAttribute('id', id);
+            const label = document.createElement('label');
+//            label.textContent = option.textContent;
+            label.innerHTML = option.innerHTML;
+            label.setAttribute('for', id);
+            radio.parentNode.insertBefore(input, radio);
+            radio.parentNode.insertBefore(label, radio);
+        }
+        radio.remove();
+    }
+}
