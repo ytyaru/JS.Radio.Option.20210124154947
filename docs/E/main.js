90399fe8fdef316ff7b12373c49e3f43a8d2bc6f
ytyaru <ytyaru@outlook.jp>
2021-01-26 14:46:58 +0900

    ColorRadio.js

--- docs/E/main.js
+++ docs/E/main.js
@@ -1,30 +1,14 @@
 window.addEventListener('load', (event) => {
     Radio.parse();
-
-    function switchDisabled(event) {
-        // <label>内<input>のdisabledを切り替える
-        for (let element of event.target.parentElement.children) {
-            if ('label' !== element.tagName.toLowerCase()) { continue; }
-            if (0 === element.children.length) { continue; }
-            if ('input' !== element.children[0].tagName.toLowerCase()) { continue; }
-            
-            if (event.target.nextSibling === element) {
-                element.children[0].removeAttribute('disabled');
-            } else {
-                element.children[0].setAttribute('disabled', 'disabled');
-            }
-        }
-    }
     // <radio id="color">にchangeイベントを実装する
-    Radio.addChangeEventListener('color', switchDisabled);
-
+    Radio.addChangeEventListener('color', ColorRadio.onChange);
     // チェックボックスに応じてイベント追加・解除する
     document.querySelector('#is-add-event').addEventListener('change', (event) => {
         console.log('is-add-event', event);
         if (event.target.checked) {
-            Radio.addChangeEventListener('color', switchDisabled);
+            Radio.addChangeEventListener('color', ColorRadio.onChange);
         } else {
-            Radio.removeChangeEventListener('color', switchDisabled);
+            Radio.removeChangeEventListener('color', ColorRadio.onChange);
         }
     });
 });
